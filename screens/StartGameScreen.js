16998cfc789417f99f31b6e886be5e5ad6696622
Naman Ajmera <cool.naman.ajmera@gmail.com>
2022-12-11 13:59:19 +0530

    Added: Style Input and validations.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,7 @@ import PrimaryButtons from '../components/PrimaryButtons'
 export default function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCapitalize='none' autoCorrect={false}/>
       <PrimaryButtons>Reset</PrimaryButtons>
       <PrimaryButtons>Confirm</PrimaryButtons>
     </View>
@@ -13,8 +13,9 @@ export default function StartGameScreen() {
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flex: 1,
     marginTop: 100,
+    marginHorizontal: 40,
+    borderRadius: 8,
     padding: 16,
     backgroundColor: '#72063c',
     elevation: 4,
@@ -22,5 +23,16 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center'
   }
 })
