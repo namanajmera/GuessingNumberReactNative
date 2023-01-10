8bdd95c7a3e818f19e70c897ced91e6488efe1b0
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-01-10 23:13:21 +0530

    Added: Gussing the number by + and -.

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,22 +1,42 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButtons from "../components/ui/PrimaryButtons";
 
-export default function GameScreen({userNumber}) {
 
-  const generateRandomBeteen = (min, max, exclude) => {
-    const rndNum = Math.floor(Math.random() * (max - min)) + min;
-    if (rndNum === exclude) {
-      return generateRandomBeteen(min, max, exclude);
-    } else {
-      return rndNum;
+let minBoundry = 1;
+let maxBoundry = 100;
+
+const generateRandomBeteen = (min, max, exclude) => {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomBeteen(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+export default function GameScreen({ userNumber }) {
+
+  const nextGuessHandler = (direction) => {
+    if ((direction === 'lower' && currentGuess < userNumber) || (direction === 'greater' && currentGuess > userNumber)) {
+      Alert.alert("Don't lie!", 'You know that this is wrong...', [
+        {text: 'Sorry!', style:'cancel'}
+      ])
+      return;
     }
+    if (direction === 'lower') {
+      maxBoundry = currentGuess;
+    } else {
+      minBoundry = currentGuess + 1;
+    }
+    const newRndNumber = generateRandomBeteen(minBoundry, maxBoundry, currentGuess);
+    setCurrentGuess(newRndNumber);
   }
 
   const [title, setTitle] = useState("Opponent's Guess");
-  const initialGuess = generateRandomBeteen(1, 100, userNumber);
-  console.log('initialGuess', initialGuess);
+  const initialGuess = generateRandomBeteen(minBoundry, maxBoundry, userNumber);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
 
   return (
@@ -25,10 +45,14 @@ export default function GameScreen({userNumber}) {
       <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or lower</Text>
+        <View>
+          <PrimaryButtons onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButtons>
+          <PrimaryButtons onPress={() => nextGuessHandler('greater')}>+</PrimaryButtons>
+        </View>
       </View>
-      <View>
+      {/* <View>
         <Text>Log Rounds</Text>
-      </View>
+      </View> */}
     </View>
   );
 }
